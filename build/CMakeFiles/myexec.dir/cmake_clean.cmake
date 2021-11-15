6a4d327906ce2d93a549c5f90c31363eac2e47b4
adrienbanse <adrien.banse@gmail.com>
2021-11-15 16:05:47 +0100

    Makefile build by cmake

---
+++ build/CMakeFiles/myexec.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myexec.dir/src/main.c.o"
+  "CMakeFiles/myexec.dir/src/inputs.c.o"
+  "bin/myexec.pdb"
+  "bin/myexec"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/myexec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
